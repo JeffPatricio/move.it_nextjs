7e9ab00b9d090057a00ba407dee7ca2f973c6438
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-02-26 08:34:06 -0300

    countdown ending, context challenges

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,24 +1,27 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
 import styles from '../styles/components/ChallengeBox.module.css';
 
 export function ChallengeBox() {
 
-  const hasActiveChallenge = true;
+  const { activeChallenge, resetChallenge } = useContext(ChallengesContext);
 
   return (
     <div className={styles.challengeBoxContainer}>
       {
-        hasActiveChallenge &&
+        !!activeChallenge &&
         <div className={styles.challengeActive}>
-          <header>Ganhe 400 xp</header>
+          <header>Ganhe {activeChallenge.amount} xp</header>
           <main>
-            <img src="icons/body.svg" alt="" />
+            <img src={`icons/${activeChallenge.type}.svg`} alt="" />
             <strong>Novo desafio</strong>
-            <p>Levante e faça uma caminhada de 3 minutos</p>
+            <p>{activeChallenge.description}</p>
           </main>
           <footer>
             <button
               type='button'
               className={styles.challengeFailedButton}
+              onClick={resetChallenge}
             >
               Falhei
               </button>
@@ -32,7 +35,7 @@ export function ChallengeBox() {
         </div>
       }
       {
-        !hasActiveChallenge &&
+        !activeChallenge &&
         <div className={styles.challengeNotActive}>
           <strong>Finalize um ciclo para receber novos desafios</strong>
           <p>
