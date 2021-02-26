611b2ed9c996cfd6b3cdf80e2bf2bc7d27c97ea6
Jefferson Patricio <jeffersonpatricio1@gmail.com>
2021-02-26 15:08:04 -0300

    modal level up

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,6 +1,7 @@
 import { createContext, useState, ReactNode, useEffect } from 'react';
 import Cookies from 'js-cookie';
 import challenges from '../../challenges.json';
+import { LevelUpModal } from '../components/LevelUpModal';
 
 interface Challenge {
   type: 'body' | 'eye';
@@ -24,6 +25,7 @@ interface ChallengesContextData {
   startNewChallenge: () => void;
   resetChallenge: () => void;
   completeChallenge: () => void;
+  closeLevelUpModal: () => void;
   activeChallenge: Challenge;
 }
 
@@ -38,6 +40,7 @@ export function ChallengesProvider({
   const [currentExperience, setCurrentExperience] = useState(rest.currentExperience ?? 0);
   const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
   const [activeChallenge, setActiveChallenge] = useState(null);
+  const [isLevelUpModalOpen, setIsLevelUpModalOpen] = useState(false);
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
@@ -53,6 +56,7 @@ export function ChallengesProvider({
 
   function levelUp() {
     setLevel(level + 1);
+    setIsLevelUpModalOpen(true);
   }
 
   function startNewChallenge() {
@@ -64,13 +68,18 @@ export function ChallengesProvider({
 
     if (Notification.permission === 'granted') {
       new Notification('Novo desafio 🎉', {
-        body: `Valendo ${challenge.amount}xp!`
+        body: `Valendo ${challenge.amount}xp!`,
+        silent: true
       });
     }
   }
 
   function resetChallenge() {
     setActiveChallenge(null);
+  }
+
+  function closeLevelUpModal() {
+    setIsLevelUpModalOpen(false);
   }
 
   function completeChallenge() {
@@ -103,10 +112,15 @@ export function ChallengesProvider({
         currentExperience,
         challengesCompleted,
         resetChallenge,
-        completeChallenge
+        completeChallenge,
+        closeLevelUpModal
       }}
     >
       {children}
+      {
+        isLevelUpModalOpen &&
+        <LevelUpModal />
+      }
     </ChallengesContext.Provider>
   );
 }
